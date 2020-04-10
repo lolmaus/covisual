34a5b1ce6ce75b370bd63bee418ee5654f011bd1
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2020-04-11 00:06:57 +0300

    Use meaningful logarithmic scale labels

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
+/* global Chart */
+
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+
 import 'chartjs-plugin-datalabels';
 
 export default class App extends Application {
@@ -11,3 +14,16 @@ export default class App extends Application {
 }
 
 loadInitializers(App, config.modulePrefix);
+
+Chart.scaleService.updateScaleDefaults('logarithmic', {
+  ticks: {
+    callback: function (...args) {
+      // https://github.com/chartjs/Chart.js/issues/3121#issuecomment-390372093
+      const value = Chart.Ticks.formatters.logarithmic.call(this, ...args);
+      if (value.length) {
+        return Number(value).toLocaleString();
+      }
+      return value;
+    },
+  },
+});
